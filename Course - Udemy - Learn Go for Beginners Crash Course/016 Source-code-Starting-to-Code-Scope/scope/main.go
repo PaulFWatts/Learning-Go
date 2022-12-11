f8a9348c02e2c0c9a783ab224a780285aede40d1
PaulFWatts <paul.watts@me.com>
2022-12-11 11:58:50 +1100

    update

---
+++ Course - Udemy - Learn Go for Beginners Crash Course/016 Source-code-Starting-to-Code-Scope/scope/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"myapp/packageone"
+)
+
+var one = "One"
+
+func main() {
+	var somethingElse = "this is a block level variable"
+	fmt.Println(somethingElse)
+	myFunc()
+
+	newString := packageone.PublicVar
+	fmt.Println("From packageone:", newString)
+
+	packageone.Exported()
+}
+
+
+func myFunc() {
+	fmt.Println(one)
+}
