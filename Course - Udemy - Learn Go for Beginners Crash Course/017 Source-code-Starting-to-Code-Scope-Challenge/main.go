bff137e42f9c9853ace740f029c705c3e5f21435
Paul Watts <paul.watts@me.com>
2022-12-13 09:22:57 +1100

    complete 017 and 018

---
+++ Course - Udemy - Learn Go for Beginners Crash Course/017 Source-code-Starting-to-Code-Scope-Challenge/main.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+// variables
+// declare a package level variable for the main
+// package named myVar
+
+// declare a block variable for the main function
+// called blockVar
+
+// declare a package level variable in the packageone
+// package named PackageVar
+
+// create an exported function in packageone called PrintMe
+
+// in the main function, print out the values of myVar,
+// blockVar, and PackageVar on one line using the PrintMe
+// function in packageone
+
+
+}
